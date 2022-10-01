7fb448a4cdce00dfb91b0efe77d860d7dfe95556
Darya-Viunyk <dar.viunyk@gmail.com>
2022-10-01 22:18:23 +0300

    gallery-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+let renderedHtml = '';
+galleryItems.forEach(element => {
+    const img = document.createElement('img');
+    img.classList.add("gallery__image");
+    img.src = element.preview;
+
+    img.alt = element.description;
+    const a = document.createElement('a');
+    a.classList.add("gallery__item");
+    a.href = element.original;
+    a.append(img);
+    renderedHtml += a.outerHTML;
+});
+
+const SimpleLightbox = window.SimpleLightbox;
+document.querySelector('.gallery').insertAdjacentHTML('beforeend', renderedHtml);
+var lightbox = new SimpleLightbox('.gallery__item', 
+{captionsData: "alt",captionDelay: 250,});
 
 console.log(galleryItems);
